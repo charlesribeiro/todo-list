04e444c09e61de3ae61ba99541168a4a4def759b
charlesribeiro <charles.r.ribeiro@gmail.com>
2021-06-19 16:47:27 -0300

    Fix test warning for custom-input.component

--- src/app/shared/custom-input/custom-input.component.spec.ts
+++ src/app/shared/custom-input/custom-input.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CustomInputComponent } from './custom-input.component';
 
@@ -8,7 +9,10 @@ describe('CustomInputComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomInputComponent ]
+      declarations: [ CustomInputComponent ],
+      imports: [
+        ReactiveFormsModule,
+      ]
     })
     .compileComponents();
   });
