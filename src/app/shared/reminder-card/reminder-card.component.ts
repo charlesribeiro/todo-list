d0586cf2b57891b86309368e9b3746b1401779ca
charlesribeiro <charles.r.ribeiro@gmail.com>
2021-07-02 21:46:10 -0300

    Add reminder-card component

---
+++ src/app/shared/reminder-card/reminder-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reminder-card',
+  templateUrl: './reminder-card.component.html',
+  styleUrls: ['./reminder-card.component.css']
+})
+export class ReminderCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { CustomInputComponent } from './custom-input/custom-input.component';
+import { ReminderCardComponent } from './reminder-card/reminder-card.component';
 
 @NgModule({
   declarations: [
     CustomInputComponent,
+    ReminderCardComponent,
   ],
   imports: [
     CommonModule,
